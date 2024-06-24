6e982233e38ab2792f4fd8831b3fa22d8cdfc047
ATHITHYA-007 <athithyaarjun27@gmail.com>
2024-06-24 14:39:58 +0530

    Updated

--- Portfolio/src/component/WorkCardData.js
+++ Portfolio/src/component/WorkCardData.js
@@ -1,25 +1,25 @@
-import pro1 from "../Assert/onlineshopping.png"
+import pro1 from "../Assert/TODO-List App.png"
 import pro2 from"../Assert/weather-app.png"
 import pro3 from "../Assert/zomato-recreation.png"
 const ProjectCardData =[
     {
         imgsrc:pro1,
-        title:"Ecommerce website Design",
-        text: "Developed an online shopping website using HTML,CSS and JavaScript Implemented local storage for efficient datamanagement ensuring a smooth and userfriendly ordering experience",
+        title:"TODO-List App",
+        text: "Developed an Todo-List Web application using HTML, CSS and JavaScript Implemented local storage for efficient datamanagement ensuring a smooth and userfriendly Web Application, where user can Update, Delete, Mark as completed and Edit the task in the list.",
         view:""
         
     },
     {
         imgsrc:pro2,
         title:"Weather-APP",
-        text: "Developed an online shopping website using HTML,CSS and JavaScript Implemented local storage for efficient datamanagement ensuring a smooth and userfriendly ordering experience",
+        text: "Developed an online shopping website using HTML, CSS and JavaScript Implemented local storage for efficient datamanagement ensuring a smooth and userfriendly ordering experience",
         view:""
         
     },
     {
         imgsrc:pro3,
         title:"Zomato-clone",
-        text: "Developed an online shopping website using HTML,CSS and JavaScript Implemented local storage for efficient datamanagement ensuring a smooth and userfriendly ordering experience",
+        text: "Developed an online shopping website using HTML, CSS and JavaScript Implemented local storage for efficient datamanagement ensuring a smooth and userfriendly ordering experience",
         view:""
         
     },
